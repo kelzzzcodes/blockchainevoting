3675c60159f90b794367f04263de9869bc2f4c46
Kelechi Otugbuali <hilakechi@gmail.com>
2022-11-12 12:06:42 +0000

    created a modal for joinpollform

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes,Route } from "react-router-dom"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import Joinpollform from "./components/Joinpollform"
+
 import Home from "./views/Home"
 import Vote from "./views/Vote"
 
@@ -13,6 +15,7 @@ const App = () => {
           <Route path="/vote/:id" element={<Vote/>}/>
       </Routes>
       
+      <Joinpollform/>
       <Footer/>
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+
+
+import{setGlobalState} from '../store/index'
 
 
 const Hero = () => {
+  
   return (
     <div className="text-center mt-10 p-4"> 
         <h1 className='text-5xl text-black-600 font-bold'> Vote Without <span className='text-blue-600'>Rigging</span></h1>
@@ -11,96 +14,21 @@ const Hero = () => {
             <button  type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Create Poll</button>
             </div>
         <div >
-        <a href={<contestantform/>}> <button type="button" class="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Join Poll</button></a>
+        <button type="button"
+         class="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+           onClick={()=> setGlobalState('boxModal','scale-100')}>Join Poll</button>
         </div>
 
-        <div class="block p-6 rounded-lg shadow-lg bg-white max-w-md">
-          <form>
-          <div class="grid grid-cols-2 gap-4">
-            
-          <div class="form-group mb-6">
-          <input type="text" class="form-control
-          block
-          w-full
-          px-3
-          py-1.5
-          text-base
-          font-normal
-          text-gray-700
-          bg-white bg-clip-padding
-          border border-solid border-gray-300
-          rounded
-          transition
-          ease-in-out
-          m-0
-          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput123"
-          aria-describedby="emailHelp123" placeholder="First name"/>
-          </div>
-          <div class="form-group mb-6">
-        <input type="text" class="form-control
-          block
-          w-full
-          px-3
-          py-1.5
-          text-base
-          font-normal
-          text-gray-700
-          bg-white bg-clip-padding
-          border border-solid border-gray-300
-          rounded
-          transition
-          ease-in-out
-          m-0
-          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput124"
-          aria-describedby="emailHelp124" placeholder="Last name"/>
-      </div>
       
 
-
-            </div>
-            <div class="form-group mb-6">
-      <input type="email" class="form-control block
-        w-full
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput125"
-        placeholder="Email address"/>
-    </div>
-    <div class="mb-3 w-96">
-    <input class="form-control
-    block
-    w-full
-    px-3
-    py-1.5
-    text-base
-    font-normal
-    text-gray-700
-    bg-white bg-clip-padding
-    border border-solid border-gray-300
-    rounded
-    transition
-    ease-in-out
-    m-0
-    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" type="file" id="formFile"/>
-  </div>
-
-          </form>
-
-        </div>
-        
+             
 
         
         
         </div>
+
+        
+        
     </div>
   )
 }
